5b794914d355f14cbe10ab056c904a6b58b2ca48
Taylor Merwin <65139555+snakefinn@users.noreply.github.com>
2023-04-08 16:02:54 -0700

    completed #191

--- src/Bits.java
+++ src/Bits.java
@@ -7,14 +7,26 @@ public class Bits {
 
   public static void main(String[] args) {
 
-    int example1 = 00000000000000000000000000001011;
+    int ex1 = 00000000000000000000000000001011;
+    int ex2 = 00000000000000000000000010000000;
+    int ex3 = -3;
 
-    hammingWeight(example1);
+    System.out.println(hammingWeight(ex1)); //should return 3
+    System.out.println(hammingWeight(ex2)); //should return 1
+    System.out.println(hammingWeight(ex3)); //should return 31
   }
 
   public static int hammingWeight(int n) {
+    int result = 0;
+    String bin = Integer.toBinaryString(n);
+    //now we have a string, we can loop through it, checking each value for a 1
 
-    return n+1;
+  for (int i = 0; i < bin.length(); i++) {
+    if (bin.charAt(i) == '1') {
+      result++;
+    }
+  }
+    return result;
   }
 
 }
